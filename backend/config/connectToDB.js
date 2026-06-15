cedc68e300eb32bcef160dd92ddf0b411272cf81
24eg107d08-venu <24eg105h11@anurag.edu.in>
2026-06-15 20:41:30 +0530

    final

--- backend/config/connectToDB.js
+++ backend/config/connectToDB.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectToDB = () => {
+  const uri = process.env.MONGO_URI;
+
+  if (!uri) {
+    throw new Error('Could not connect to MongoDB: MONGO_URI is not defined. Check your .env file.');
+  }
+
   mongoose
-    .connect(process.env.MONGO_DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(uri)
     .then(() => {
       console.log('Connected to MongoDB');
     })
